e158fe70c12d2cd9ee2454b10cc0957b35d14490
kristofke1 <88692710+kristofke1@users.noreply.github.com>
2026-01-03 01:09:53 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Altered Lore",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
